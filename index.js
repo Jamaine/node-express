00c62e1b05d02e71c00605bc07d46cf3f68a09f6
jamaine <jamaine.spooner@gmail.com>
2016-05-26 15:49:33 +0100

    lesson 6 complete

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const _ = require('lodash');
 const engines = require('consolidate');
 const bodyParser = require('body-parser');
 
+// returns the .json file filepath for a specified user, based on their username
 function getUserFilePath(username) {
   return path.join(__dirname, 'users', username) + '.json';
 }
@@ -59,10 +60,43 @@ app.get('/', (req, res) => {
   })
 })
 
+function verifyUser(request, response, next) {
+  const filepath = getUserFilePath(request.params.username);
+  // check if the file exists at filepath exists
+  fs.exists(filepath, function(yes) {
+    // returns boolean
+    if(yes) {
+      // if true go to next
+      // next is the next function, in this case is the anonymous function after verifyUser is called
+      next()
+    } else {
+      // redirect to our error route
+      response.redirect('/error/' + request.params.username);
+    }
+  })
+}
+
+app.get('*.json', (request, response) => {
+  //  allows us to download files, takes an optional second parameter, if you want to give the file a different name
+  response.download(`./users/${request.path}`)
+})
+
+// sends json response to the browser
+// useful for creating an api server
+app.get('/data/:username', (request, response) => {
+  const username = request.params.username;
+  const user = getUser(username);
+  response.json(user);
+})
+
+app.all('/:username', (req, res, next) => {
+  console.log(req.method, 'for', req.params.username);
+  next();
+})
 
 // ":" tells express that `username` is a path variable
 // /:username could be domain/anything
-app.get('/:username', (req, res) => {
+app.get('/:username', verifyUser, (req, res) => {
   const username = req.params.username;
   const user = getUser(username);
   // The file user.hbs
@@ -71,6 +105,12 @@ app.get('/:username', (req, res) => {
     user: user,
     address: user.location
   })
+})
+
+//
+app.get('/error/:username', (request, response) => {
+  response.status(404) // optionally send a 404 status as a page response
+    .send(`no user named ${request.params.username} found`)
 })
 
 app.put('/:username', (req, res) => {
