6f4693c3258db08bcef7571cb4c207c7da220f37
Jamaine Spooner <jamaine.spooner@gmail.com>
2016-05-26 21:28:31 +0100

    lesson 6 - Removal of comments

--- username.js
+++ username.js
@@ -47,36 +47,3 @@ router.delete('/', (req, res) => {
 });
 
 module.exports = router;
-
-// app.route('/:username')
-// .all((req, res, next) => {
-//   console.log(req.method, 'for', req.params.username);
-//   next();
-// })
-// .get(helpers.verifyUser, (req, res) => {
-//   const username = req.params.username;
-//   const user = helpers.getUser(username);
-//   // The file user.hbs
-//   // Properties we want to be availble to the view
-//   res.render('user', {
-//     user: user,
-//     address: user.location
-//   })
-// })
-// .put((req, res) => {
-//   const username = req.params.username;
-//   const user = helpers.getUser(username);
-//   // data object which is passed back from the form
-//   user.location = req.body;
-//
-//   helpers.saveUser(username, user);
-//   // end the request
-//   res.end();
-// })
-// .delete((req, res) => {
-//   const fp = helpers.getUserFilePath(req.params.username);
-//   // deletes file
-//   fs.unlinkSync(fp);
-//   // send a 200 status back to the client
-//   res.sendStatus(200);
-// })
